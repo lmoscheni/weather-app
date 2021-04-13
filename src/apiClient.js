d548c2f7d94b23624f93b68f74d7f1dfa6265362
Leandro Moscheni <leandromoscheni@gmail.com>
2021-04-13 15:09:46 -0300

    create basic api client

---
+++ src/apiClient.js
@@ -0,0 +1,18 @@
+import axios from "axios";
+
+const SEARCH_LOCATION_URL = "https://www.metaweather.com/api/location/search/";
+const WEATHER_URL = "https://www.metaweather.com/api/location";
+
+export function findLocationByCoords(latitude, longitude) {
+  return axios
+    .get(
+      `http://localhost:8080/${SEARCH_LOCATION_URL}?lattlong=${latitude},${longitude}`
+    )
+    .then((response) => response.data[0]);
+}
+
+export function getWeatherInfoByLocationId(locationId) {
+  return axios
+    .get(`http://localhost:8080/${WEATHER_URL}/${locationId}`)
+    .then((response) => response.data);
+}
