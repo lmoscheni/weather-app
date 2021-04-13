11942c95dc42e5940a0ac9934e591ca5c629227f
Leandro Moscheni <leandromoscheni@gmail.com>
2021-04-13 19:27:25 -0300

    use cors-enable service to consume weather api

--- src/apiClient.js
+++ src/apiClient.js
@@ -2,23 +2,34 @@ import axios from "axios";
 
 const SEARCH_LOCATION_URL = "https://www.metaweather.com/api/location/search/";
 const WEATHER_URL = "https://www.metaweather.com/api/location";
+const CORS_ENABLE_URL = "https://cors-anywhere.herokuapp.com";
+
+const corsHeaders = {
+  origin: window.location.href,
+  "x-requested-with": "XmlHttpRequest",
+};
 
 export function findLocationByCoords(latitude, longitude) {
   return axios
     .get(
-      `http://localhost:8080/${SEARCH_LOCATION_URL}?lattlong=${latitude},${longitude}`
+      `${CORS_ENABLE_URL}/${SEARCH_LOCATION_URL}?lattlong=${latitude},${longitude}`,
+      { header: corsHeaders }
     )
     .then((response) => response.data[0]);
 }
 
 export function findLocationByQuery(query) {
   return axios
-    .get(`http://localhost:8080/${SEARCH_LOCATION_URL}?query=${query}`)
+    .get(`${CORS_ENABLE_URL}/${SEARCH_LOCATION_URL}?query=${query}`, {
+      header: corsHeaders,
+    })
     .then((response) => response.data);
 }
 
 export function getWeatherInfoByLocationId(locationId) {
   return axios
-    .get(`http://localhost:8080/${WEATHER_URL}/${locationId}`)
+    .get(`${CORS_ENABLE_URL}/${WEATHER_URL}/${locationId}`, {
+      header: corsHeaders,
+    })
     .then((response) => response.data);
 }
