fd2c9bcc489adfad1c1120c9f37cb63c7ed0f146
Leandro Moscheni <leandromoscheni@gmail.com>
2021-04-13 18:56:17 -0300

    Add new method into apiClient

--- src/apiClient.js
+++ src/apiClient.js
@@ -11,6 +11,12 @@ export function findLocationByCoords(latitude, longitude) {
     .then((response) => response.data[0]);
 }
 
+export function findLocationByQuery(query) {
+  return axios
+    .get(`http://localhost:8080/${SEARCH_LOCATION_URL}?query=${query}`)
+    .then((response) => response.data);
+}
+
 export function getWeatherInfoByLocationId(locationId) {
   return axios
     .get(`http://localhost:8080/${WEATHER_URL}/${locationId}`)
