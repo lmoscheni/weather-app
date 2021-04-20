763723cc70233cf0a957e28e4b6fbd519722a2dd
Leandro Moscheni <leandromoscheni@gmail.com>
2021-04-20 16:47:25 -0300

    create WindIcon component

---
+++ src/components/WeatherDetail/HightLights/WindIcon/index.js
@@ -0,0 +1,9 @@
+import "./styles.css";
+
+export default function WindIcon() {
+  return (
+    <div className="wind-icon-container">
+      <span class="icon material-icons">navigation</span> WSP
+    </div>
+  );
+}
