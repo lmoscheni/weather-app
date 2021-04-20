167b708dbfdb18ac2a911e630964254761ece3a1
Leandro Moscheni <leandromoscheni@gmail.com>
2021-04-20 15:55:25 -0300

    create Footer component for WeatherDetail component

---
+++ src/components/WeatherDetail/Footer/index.js
@@ -0,0 +1,12 @@
+import "./styles.css";
+
+export default function Footer() {
+  return (
+    <footer className="footer-container">
+      <p className="footer-content">
+        created by <span className="user-info">lmoscheni</span> -
+        devChallenges.io
+      </p>
+    </footer>
+  );
+}
